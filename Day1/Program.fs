cc95a5926ac3b6ad149a6d36ee1ca595bd9abb7d
Dewald Haasbroek (ZA) <dewald.haasbroek@pwc.com>
2026-01-16 13:32:17 +0200

    Day 1 Part 2 Complete Day 7 Part 1 & 2 Complete

--- Day1/Program.fs
+++ Day1/Program.fs
@@ -1,54 +1,80 @@
-﻿
+﻿type Rotation = | Left of int | Right of int
+    with 
+        member this.GetRotationCount = 
+            match this with 
+            | Left i -> i  | Right i -> i
+
+        static member parseX (s: string) = 
+            match s[0] with 
+            | 'L' -> Left (int s[1..])
+            | 'R' -> Right (int s[1..])
+            | c -> failwith $"Invalid direction recieved : {c}"
+
+type State = {CurrentPos : int; ZeroCount : int}
+
+let inputData =
+    System.IO.File.ReadAllText("./input.txt").Split('\n')
+    |> Array.toList
+
+let testInput = [ "L68"; "L30"; "R48"; "L5"; "R60"; "L55"; "L1"; "L99"; "R14"; "L82"]
+
+let calculateNextPos (rotation : Rotation) (currentPos : int)= 
+    match rotation with 
+    | Left  r -> (currentPos - (r % 100) + 100) % 100
+    | Right r -> (currentPos + (r % 100)) % 100
+
+let calculateZeroRestCount (s : State) (rotation : Rotation) : State = 
+    let newPos = calculateNextPos rotation s.CurrentPos
+    if newPos = 0 then 
+        {s with CurrentPos = newPos; ZeroCount = s.ZeroCount + 1}
+    else 
+        {s with CurrentPos = newPos}
+    
+let rotationToSignedValue (rotation: Rotation) =
+    match rotation with
+    | Left r -> -r
+    | Right r -> r
+
+let calculateZeroPassCount (s : State) (rotation : Rotation) : State = 
+    let modulus = 100
+    let clicks = rotationToSignedValue rotation
+    let full_spins = abs(clicks / modulus) 
+    let rem_clicks = clicks % modulus
+    let currentPos = s.CurrentPos
+    
+    let nsum = currentPos + rem_clicks
+
+    let zeroPassedPartial = 
+        if (nsum >= modulus) || (nsum <= 0 && currentPos > 0) then true
+        else false
+
+    let zerosAdded =
+        if zeroPassedPartial then full_spins + 1 else full_spins
+
+    let newPos = ((nsum % modulus) + modulus) % modulus
+
+    { s with CurrentPos = newPos; ZeroCount = s.ZeroCount + zerosAdded }
+
+let part_1 (input : List<string>) = 
+    input
+    |> List.map Rotation.parseX
+    |> List.fold calculateZeroRestCount {CurrentPos = 50; ZeroCount = 0}
+    |> _.ZeroCount
+
+let part_2 (input : List<string>) = 
+    input
+    |> List.map Rotation.parseX
+    |> List.fold calculateZeroPassCount {CurrentPos = 50; ZeroCount = 0}
+    |> _.ZeroCount
+
 [<EntryPoint>]
 let main _args = 
 
-    let inputLines =
-        System.IO.File.ReadAllText("./input.txt").Split('\n')
-        |> Array.toList
-
-    let isInvalidId (n: int64) =
-        let s = string n
-        let len = s.Length
-        if len % 2 <> 0 then
-            false
-        else
-            let half = len / 2
-            let first = s.Substring(0, half)
-            let second = s.Substring(half, half)
-            first = second
-
-    let parseRange (text: string) : int64 * int64 =
-        let parts = text.Split('-')
-        if parts.Length <> 2 then
-            failwithf "Invalid range: '%s'" text
-        let lo = int64 parts[0]
-        let hi = int64 parts[1]
-        lo, hi
-
-    let sumInvalidInRange (lo: int64, hi: int64) : int64 =
-        if hi < lo then 0L
-        else
-            let count = hi - lo + 1L
-            if count > int64 System.Int32.MaxValue then
-                failwithf "Range too large to enumerate: %d-%d" lo hi
-            else
-                Seq.init (int count) (fun i -> lo + int64 i)
-                |> Seq.filter isInvalidId
-                |> Seq.sum
-
-    let totalInvalidSum =
-        inputLines
-        |> List.collect (fun line ->
-            line.Split(',', System.StringSplitOptions.RemoveEmptyEntries)
-            |> Array.toList)
-        |> List.map (fun s -> s.Trim())
-        |> List.filter (fun s -> s <> "")
-        |> List.map parseRange
-        |> List.map sumInvalidInRange
-        |> List.sum
-
-    printfn "%d" totalInvalidSum
-
+    // Part one 1029
+    printf $"Part 1 Test : {part_1 testInput}\n"
+    printf $"Part 1      : {part_1 inputData}\n"
+    printf $"Part 2 Test : {part_2 testInput}\n"
+    printf $"Part 2      : {part_2 inputData}\n"
     0
 
 
---
+++ Day7/Program.fs
@@ -0,0 +1,108 @@
+﻿let testInput = """.......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............
+"""
+
+
+let inputData = System.IO.File.ReadAllText("./input.txt")
+let parse (s : string) = 
+    s.Split([|"\n"; "\r"|],System.StringSplitOptions.RemoveEmptyEntries )
+    |> Array.map (fun s -> s |> seq |> Seq.toArray)
+
+let startingColumn (grid : array<array<char>>) = 
+    grid[0]
+    |> Array.findIndex(fun c -> c = 'S')
+
+let calculateSplitCount (grid: array<array<char>>) = 
+    
+    let start = startingColumn grid
+
+    let rec calc (beamMap : Map<int * int, int>) (row : int) (splitCount : int) = 
+        if row < grid.Length - 1 then
+            let mutable nextBeamMap = beamMap
+            let mutable rowSplitCount = splitCount
+            
+            grid[row]
+            |> Array.iteri (fun col v -> 
+                match beamMap |> Map.tryFind (col, row - 1) with 
+                | Some _ ->
+                    if v = '^' then
+                        nextBeamMap <- nextBeamMap |> Map.add (col - 1, row) 1
+                        nextBeamMap <- nextBeamMap |> Map.add (col + 1, row) 1
+                        rowSplitCount <- rowSplitCount + 1
+                    else 
+                        nextBeamMap <- nextBeamMap |> Map.add (col, row) 1
+                | None -> ()
+            )
+            calc nextBeamMap (row + 1) rowSplitCount
+        else 
+            splitCount
+
+    calc ([(start, 1), 1] |> Map.ofList) 1 0
+
+let part_1 (input : string) = 
+    input
+    |> parse
+    |> calculateSplitCount
+
+let countLeaves startCol (grid: char[][]) =
+    let rows = grid.Length
+    let cols = if rows > 0 then grid.[0].Length else 0
+
+    let ways = Array2D.create cols rows 0L
+
+    ways.[startCol, 0] <- 1L 
+
+    for row in 0 .. rows - 1 do
+      for col in 0 .. cols - 1 do
+        let count = ways.[col,row]
+        if count > 0L then
+          if row = rows - 1 then ()
+          else
+            match grid.[row].[col] with
+            | '^' ->
+                if col - 1 >= 0 then 
+                    ways.[col - 1, row + 1] <- ways.[col - 1, row + 1] + count
+                if col + 1 < cols then 
+                    ways.[col + 1, row + 1] <- ways.[col + 1, row + 1] + count
+            | 'S' | '|' | '.' | ' ' ->
+                ways.[col, row + 1] <- ways.[col, row + 1] + count
+            | _ -> ()
+
+    let mutable leafCount = 0L
+
+    for col in 0 .. cols - 1 do
+        match grid.[rows - 1].[col] with
+        | '^' ->
+            leafCount <- leafCount + ways.[col, rows - 1] * 2L
+        | 'S' | '|' | '.' | ' ' ->
+            leafCount <- leafCount + ways.[col, rows - 1]
+        | _ ->
+            leafCount <- leafCount + ways.[col, rows - 1]
+
+    leafCount
+
+let part_2 (input : string) = 
+    let grid = input |> parse
+    countLeaves (startingColumn grid) grid
+
+[<EntryPoint>]
+let main _ = 
+    printf $"Part 1 Test : %i{part_1 testInput}\n"
+    printf $"Part 1      : %i{part_1 inputData}\n"
+    printf $"Part 2 Test : %i{part_2 testInput}\n"
+    printf $"Part 2      : %i{part_2 inputData}\n"
+    0
